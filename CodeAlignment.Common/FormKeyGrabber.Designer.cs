c66836b48de6ed6a56ec162d6409df8a59e9618a
Chris McGrath <dev@cpmcgrath.com>
2013-03-19 16:42:54 +1000

    Added icon and cleaned the form a bit. Closes #16

--- CodeAlignment.Common/FormKeyGrabber.Designer.cs
+++ CodeAlignment.Common/FormKeyGrabber.Designer.cs
@@ -29,32 +29,48 @@
         private void InitializeComponent()
         {
             System.Windows.Forms.Label lblDescription;
+            System.Windows.Forms.PictureBox icon;
             lblDescription = new System.Windows.Forms.Label();
+            icon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(icon)).BeginInit();
             this.SuspendLayout();
             // 
             // lblDescription
             // 
             lblDescription.Dock = System.Windows.Forms.DockStyle.Fill;
-            lblDescription.Location = new System.Drawing.Point(0, 0);
+            lblDescription.Location = new System.Drawing.Point(106, 0);
             lblDescription.Name = "lblDescription";
-            lblDescription.Size = new System.Drawing.Size(450, 20);
+            lblDescription.Size = new System.Drawing.Size(394, 20);
             lblDescription.TabIndex = 0;
-            lblDescription.Text = "Code alignment started, waiting for second shortcut key";
+            lblDescription.Text = "Waiting for second key of chord...";
             lblDescription.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // icon
+            // 
+            icon.Dock = System.Windows.Forms.DockStyle.Left;
+            icon.Image = global::CMcG.CodeAlignment.Properties.Resources.CodeAlignmentBanner96x16;
+            icon.Location = new System.Drawing.Point(0, 0);
+            icon.Name = "icon";
+            icon.Size = new System.Drawing.Size(106, 20);
+            icon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            icon.TabIndex = 1;
+            icon.TabStop = false;
             // 
             // FormKeyGrabber
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 20);
+            this.ClientSize = new System.Drawing.Size(500, 20);
             this.Controls.Add(lblDescription);
+            this.Controls.Add(icon);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.MaximumSize = new System.Drawing.Size(450, 20);
+            this.MaximumSize = new System.Drawing.Size(500, 80);
             this.Name = "FormKeyGrabber";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "FormKeyGrabber";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnKeyDown);
+            ((System.ComponentModel.ISupportInitialize)(icon)).EndInit();
             this.ResumeLayout(false);
 
         }
