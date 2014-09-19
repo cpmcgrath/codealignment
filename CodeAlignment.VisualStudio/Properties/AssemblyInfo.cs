468d33378d60e9b5faf156badee34938863d19fd
Chris McGrath <admin@cpmcgrath.com>
2014-09-19 10:09:37 +1000

    #26 New Description

--- CodeAlignment.VisualStudio/Properties/AssemblyInfo.cs
+++ CodeAlignment.VisualStudio/Properties/AssemblyInfo.cs
@@ -8,9 +8,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle           ("Code alignment for Visual Studio")]
-[assembly: AssemblyDescription     ("Code alignment is a practice for formating your code.\r\n"
-                                  + "Based on principals from maths and other displines, code alignment gives extra meaning to your code by lining up similar code parts into columns.\r\n"
-                                  + "This is a practice everyone performs on most data using tables and spreadsheets, but unfortunately doesn't get applied to code often enough.")]
+[assembly: AssemblyDescription     ("Code alignment helps you present your code beautifully, enhancing clarity and readability.")]
 [assembly: AssemblyConfiguration   ("")]
 [assembly: AssemblyCompany         ("Chris McGrath")]
 [assembly: AssemblyProduct         ("Code alignment")]
