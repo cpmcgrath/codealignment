b2b9e92778b64a6d7e135d2d3e8aed0bde4aa3af
Chris McGrath <admin@cpmcgrath.com>
2018-01-04 14:59:56 +1000

    Updated version to 14. Closes #64

--- CodeAlignment.VisualStudio/Properties/AssemblyInfo.cs
+++ CodeAlignment.VisualStudio/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("13.1.0.*")]
-[assembly: AssemblyFileVersion("13.1.0.0")]
+[assembly: AssemblyVersion("14.0.0.*")]
+[assembly: AssemblyFileVersion("14.0.0.0")]
